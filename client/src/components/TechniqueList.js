9eb0be0bc800168dc72d42ce8e9600c135e21397
Elvis Ibarra <ielvisd@gmail.com>
2019-04-28 21:11:27 -0700

    adding user message upon login

--- client/src/components/TechniqueList.js
+++ client/src/components/TechniqueList.js
@@ -8,7 +8,10 @@ const TechniqueList = () => {
 
 	return (
 		<HeaderContainer>
-			<p>Welcome to jiujitsio!</p>
+			<p>
+				Welcome to jiujitsio {state.name}! Thanks for checking it out! Come back soon to see new features!
+				You'll be able to schedule and track mat time, techniques & sparring stats. See you next week!
+			</p>
 		</HeaderContainer>
 	);
 };
